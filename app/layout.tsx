6882958e01c1da907c2e06e4d9aa7cd38b75a365
lukesokol <lukaszsokol97@gmail.com>
2024-02-27 22:51:21 +0100

    Particles fixes #3

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,11 +8,6 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Fira_Code } from "next/font/google";
-import { useEffect, useState } from "react";
-import Particles, { initParticlesEngine } from "@tsparticles/react";
-import { loadFull } from "tsparticles";
-import particlesConfig from "../config/particles-config";
-import { ISourceOptions } from "@tsparticles/engine";
 import ParticlesBackground from "@/components/particles-background";
 
 //👇 Configure our font object
@@ -34,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body
-        className={`${firaCode.variable} bg-slate-50 text-slate-950 relative pt-28 sm:pt-36 dark:bg-slate-900 dark:text-slate-50 dark:text-opacity-90`}
+        className={`${firaCode.variable} !bg-slate-50 text-slate-950 relative pt-28 sm:pt-36 !dark:bg-slate-900 dark:text-slate-50 dark:text-opacity-90`}
       >
         <ThemeContextProvider>
           <ParticlesBackground />
