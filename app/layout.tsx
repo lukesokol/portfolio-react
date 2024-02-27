dba8558c0d4e462ca4fd103a585869c140da3b6b
lukesokol <lukaszsokol97@gmail.com>
2024-02-27 23:10:34 +0100

    Reverted particles changes due to problems on iOS

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,7 +8,11 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Fira_Code } from "next/font/google";
-import ParticlesBackground from "@/components/particles-background";
+import { useEffect, useState } from "react";
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { loadFull } from "tsparticles";
+import particlesConfig from "../config/particles-config";
+import { ISourceOptions } from "@tsparticles/engine";
 
 //👇 Configure our font object
 const firaCode = Fira_Code({
@@ -18,21 +22,31 @@ const firaCode = Fira_Code({
 });
 
 // Temporary solution - to be changed based on the theme
-// var particlesConfigMod = JSON.parse(JSON.stringify(particlesConfig));
-// particlesConfigMod.background.color = "";
+var particlesConfigMod = JSON.parse(JSON.stringify(particlesConfig));
+particlesConfigMod.background.color = "";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadFull(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
   return (
     <html lang="en" className="!scroll-smooth">
+      {init && <Particles options={particlesConfigMod as ISourceOptions} />}
       <body
         className={`${firaCode.variable} bg-slate-50 text-slate-950 relative pt-28 sm:pt-36 dark:bg-slate-900 dark:text-slate-50 dark:text-opacity-90`}
       >
         <ThemeContextProvider>
-          <ParticlesBackground />
           <ActiveSectionContextProvider>
             <Header />
             {children}
